393c225afeb1293c48c82c51f1a7d0c75a8c25da
aisyah-h <83572065+aisyah-h@users.noreply.github.com>
2021-05-23 15:33:57 +0800

    edited after Angela's solution

--- src/index.js
+++ src/index.js
@@ -10,13 +10,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+{
+  /*My attempt:*/
+}
 const name = "Aisyah";
 const year = new Date().getFullYear();
+
+{
+  /* Angela's currentDate method: 
+- logs to console the current date
+*/
+}
+const currentDate = new Date();
+console.log(currentDate);
+const year1 = currentDate.getFullYear();
 
 ReactDOM.render(
   <div>
     <p>Created by {name}.</p>
     <p>Copyright {year}.</p>
+
+    <p>Created by {name}.</p>
+    <p>Copyright {year1}.</p>
   </div>,
   document.getElementById("root")
 );
