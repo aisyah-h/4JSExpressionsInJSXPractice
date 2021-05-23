ae3cd82146c32fcfbb287b60aaa33a55ec677758
aisyah-h <83572065+aisyah-h@users.noreply.github.com>
2021-05-23 15:14:36 +0800

    Added in challenge code

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Aisyah";
+const year = 2021;
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}.</p>
+    <p>Copyright {year}.</p>
+  </div>,
+  document.getElementById("root")
+);
